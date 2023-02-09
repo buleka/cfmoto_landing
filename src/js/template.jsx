3140c4a620abe318aabe850acbc012856bfa0410
olga <buleka>
2023-02-09 16:23:05 +0300

    add project

---
+++ src/js/template.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+class Template extends React.Component {
+    render() {
+        return (
+            <div>
+                <main>
+                    <div className={'container'}>
+
+
+                    </div>
+                </main>
+            </div>
+        )
+    }
+
+}
+
+export default Template;
